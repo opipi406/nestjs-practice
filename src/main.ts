8e6179a67ca2841cd9b1a9a3c4832559bf305d9d
opipi406 <opipi.work@gmail.com>
2023-12-08 01:45:28 +0900

    firebase functionsデプロイ対応

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { TransformInterceptor } from 'src/interceptor/transform.interceptor';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
